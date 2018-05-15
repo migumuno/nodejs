10fe62365160c2c24de5b19c69555918fffe16d4
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-05-15 09:30:36 +0200

    Añado callback a la función para que cada ejecución respete el setTimeout.

--- js_avanzado/asincrono.js
+++ js_avanzado/asincrono.js
@@ -3,11 +3,16 @@
 console.log( 'Empiezo' );
 
 // función que escribe un texto tras 2 segundos
-function escribeTras2Segundos(texto) {
+function escribeTras2Segundos(texto, callback) {
     setTimeout( () => { // declaración simplificada de función
         console.log( texto );
+        callback(22);
     }, 2000 );
 }
 
-escribeTras2Segundos( 'Texto 1' );
-escribeTras2Segundos( 'Texto 2' );
+escribeTras2Segundos( 'Texto 1', () => {
+    console.log( 'Termino la primera ejecución.' );
+    escribeTras2Segundos( 'Texto 2', () => {
+        console.log( 'Termino la segunda ejecución.' );
+    } );
+} );
