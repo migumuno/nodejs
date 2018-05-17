77e7889080688625ec133308caf4e0e8af953dd9
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-05-17 10:39:59 +0200

    Anotación de aclaración de archivos estáticos en app.js

--- express/nodeapi/app.js
+++ express/nodeapi/app.js
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// middleware de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
