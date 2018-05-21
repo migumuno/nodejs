7a2e0dc79cb9075ca0b8a3a1b75bab61f76ab58d
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-05-21 12:11:05 +0200

    Ejemplo de módulo MYSQL conectando a BBDD.

---
+++ mysql/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// cargar el driver
+const mysql = require('mysql');
+
+// crear una conexión
+const connection = mysql.createConnection({
+    host: 'didimo.es',
+    user: 'usuariocurso',
+    password: 'us3r',
+    database: 'cursonode'
+});
+
+// conectar
+connection.connect( err => {
+    if(err) {
+        console.error( 'Imposible conectar', err );
+        process.exit(1);
+    }
+
+    // lanzar consulta
+    connection.query( 'SELECT * FROM agentes', (err, rows, fields) => {
+        if (err) {
+            console.error( 'Imposible hacer consulta', err );
+            process.exit(1);
+        }
+
+        console.log(rows);
+    
+        // cerrar conexión
+        connection.end();
+    } );
+} );
---
+++ mysql/info.md
@@ -0,0 +1,16 @@
+# MYSQL
+Para conectar a MYSQL es bastante sencillo, únicamente es necesario instalar el módulo de mysql con npm.
+
+*npm i mysql*
+
+El ejemplo de cómo hacer una conexión está en index.js.
+
+## Bases de datos - SQL ORMs
+Los ORMs (Object Relational Mapping) se encarga de:
+- Convertir objetos en constulas SQL.
+- Traducir los resultados de una consulta SQL y generar objetos.
+
+Ejemplos de ORMs:
+- sequelizejs
+- Knex
+- Bookshelf
