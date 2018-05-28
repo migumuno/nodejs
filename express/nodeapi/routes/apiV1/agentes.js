883fe22072143bf11ee3b1a2c992864f9b484735
Miguel Ángel Muñoz Viejo <miguelangelmunozviejo@MacBook-Pro-de-Miguel.local>
2018-05-28 11:11:46 +0200

    Añadida ruta para creación de agentes

--- express/nodeapi/routes/apiV1/agentes.js
+++ express/nodeapi/routes/apiV1/agentes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const Agente = require('../../models/Agente');
 
+// Obtener agentes
 router.get('/', (req, res, next) => {
     Agente.find().exec( (err, docs) => {
         if(err) {
@@ -14,6 +15,25 @@ router.get('/', (req, res, next) => {
 
         // sino hay error
         res.json({ success: true, result: docs });
+    } );
+});
+
+// Crear agentes
+router.post('/', (req, res, next) => {
+    console.log( req.body );
+    const data = req.body;
+
+    // Creamos documento de agente en memoria
+    const agente = new Agente(data);
+
+    // Lo guardamos en la BBDD
+    agente.save( (err, agenteGuardado) => {
+        if(err) {
+            next(err);
+            return;
+        }
+
+        res.json({ success: true, result: agenteGuardado });
     } );
 });
 
